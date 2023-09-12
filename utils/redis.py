a1c4251eae70379cb625ef41c7ad5884ddf9bb7b
Mehul Chattopadhyay <mehul.chattopadhyay@tuteck.com>
2023-09-12 17:47:04 +0530

    Appending to redis sorted set

--- utils/redis.py
+++ utils/redis.py
@@ -1,20 +1,29 @@
+from config.redis import redis
+
 class Redis:
 
 
-    async def update(sorted_set:str,transporter_id:str,rate:float) -> (any,str):
+    async def update(self,sorted_set:str,transporter_id:str,rate:float) -> (any,str):
+        if self.exists(sorted_set=sorted_set,key=transporter_id):
+            redis.zadd(sorted_set,{transporter_id : rate},xx=True)
+        
+        redis.zadd(sorted_set,{transporter_id : rate})
+
+    async def get_first(self,sorted_set:str):
         pass
 
-    async def get_first(sorted_set:str):
+    async def get_last(self,sorted_set:str):
         pass
 
-    async def get_last(sorted_set:str):
+    async def get_first_n(self,sorted_set : str, n : int):
         pass
 
-    async def get_first_n(sorted_set : str, n : int):
+    async def get_last_n(self,sorted_set : str, n : int):
         pass
 
-    async def get_last_n(sorted_set : str, n : int):
-        pass
+    async def exists(self,sorted_set : str,key : str) -> bool:
+        if not redis.zscore(sorted_set, key):
+            return False
+        return True
 
-    async def exists(sorted_set : str) -> bool:
-        pass
+        
