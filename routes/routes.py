e8f1ebd51d635fee9c9bc8830a1bd0241c4b2576
Mehul Chattopadhyay <mehul.chattopadhyay@tuteck.com>
2023-09-29 18:18:23 +0530

    All user ids, shipper ids and transporter ids will now come from token

--- routes/routes.py
+++ routes/routes.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, FastAPI
 
 from routes.bidding import bidding_router
+from middleware.auth import AuthMiddleware
 
 router: APIRouter = APIRouter(prefix="/api/v1")
 
 
 def setup_routes(app: FastAPI):
+
+    # app.add_middleware(AuthMiddleware)
 
     router.include_router(bidding_router)
 
